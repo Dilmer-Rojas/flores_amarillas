b3271127451c0d41d52cc97272ff6903d3ca03ac
Dilmer-Rojas <dilmer.rojas@ukuepa.com>
2024-09-21 19:08:04 -0500

    firth

---
+++ src/js/redirigir.js
@@ -0,0 +1,9 @@
+function mostrarMensaje() {
+    document.getElementById("mensaje").style.display = "block"; // Muestra el mensaje
+    document.getElementById("fondo").style.display = "block"; // Muestra el fondo
+}
+
+// function ocultarMensaje() {
+//     document.getElementById("mensaje").style.display = "none"; // Oculta el mensaje
+//     document.getElementById("fondo").style.display = "none"; // Oculta el fondo
+// }
